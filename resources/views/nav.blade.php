45cbebea2320256d54ade5519c7c3156d5642caf
Lucia Ma <lucia.ma@wundermanthompson.com>
2022-05-17 17:02:56 +0200

    Users page

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,10 +1,10 @@
-<nav class="bg-white border-b">
+<nav class="bg-white border-b" id="nav">
     <div class="container flex items-center max-w-7xl mx-auto py-4">
         <div class="self-center text-xl font-semibold whitespace-nowrap">SSL Automation</div>
         <div class="w-full md:block md:w-auto ml-6">
             <ul class="flex mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                 <li>
-                    <a href="/certificates" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" aria-current="page">Certificates</a>
+                    <a href="/certificates" class="block py-2 pr-4 pl-3 text-gray-700 bg-blue-700 rounded md:bg-transparent md:p-0" aria-current="page">Certificates</a>
                 </li>
                 <li>
                     <a href="/environments" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Environments</a>
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -26,6 +26,80 @@
             </div>
         </header>
 
+        <div class="pt-6 pb-12">
+            <div class="max-w-7xl mx-auto">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class="bg-white border-b border-gray-200">
+
+                        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3" width="30%">
+                                            NAME
+                                        </th>
+                                        <th scope="col" class="px-6 py-3" width="40%">
+                                            EMAIL
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            ACCOUNT STATUS
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="bg-white border-b hover:bg-gray-50">
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                            (name)
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            (email)
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            (status)
+                                        </td>
+                                        <td class="px-6 py-4 text-right">
+                                            <a href="/certificate-details" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Reset link</a>
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white border-b hover:bg-gray-50">
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                            (name)
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            (email)
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            (status)
+                                        </td>
+                                        <td class="px-6 py-4 text-right">
+                                            <a href="/certificate-details" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Reset link</a>
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white hover:bg-gray-50">
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                            (name)
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            (email)
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            (status)
+                                        </td>
+                                        <td class="px-6 py-4 text-right">
+                                            <a href="/certificate-details" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Reset link</a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </body>
 
