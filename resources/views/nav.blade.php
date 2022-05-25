58743f264be123e6d59d7e413a28f20285539a67
Lucia Ma <lucia.ma@wundermanthompson.com>
2022-05-25 16:04:08 +0200

    Nav bar update

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-white border-b" id="nav">
     <div class="container flex items-center max-w-7xl mx-auto py-4">
         <div class="self-center text-xl font-semibold whitespace-nowrap">SSL Automation</div>
-        <div class="w-full md:block md:w-auto ml-6">
+        <div class="w-full flex justify-between ml-6">
             <ul class="flex mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                 <li>
                     <a href="/certificates" class="block py-2 pr-4 pl-3 text-gray-700 bg-blue-700 rounded md:bg-transparent md:p-0" aria-current="page">Certificates</a>
@@ -11,6 +11,12 @@
                 </li>
                 <li>
                     <a href="/users" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Users</a>
+                </li>
+            </ul>
+            <ul class="flex mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                <li>User Name</li>
+                <li>
+                    <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Log out</a>
                 </li>
             </ul>
         </div>
