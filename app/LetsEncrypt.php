2cc4f8500e8435bb0de6c6238d4bc7de53129567
sidneydesousa <>
2022-12-09 11:46:02 +0200

    shell exec response

--- app/LetsEncrypt.php
+++ app/LetsEncrypt.php
@@ -64,9 +64,9 @@ class LetsEncrypt
     public function certificateAuthorization($mainDomain, $additionalDomains = "")
     {
         // dd(env('ROOT_DIR') . "authorize {$mainDomain} {$additionalDomains} -n");
-        $authResponse = exec(env('ROOT_DIR') . "authorize {$mainDomain} {$additionalDomains} -n");
+        $authResponse = shell_exec(env('ROOT_DIR') . "authorize {$mainDomain} {$additionalDomains} -n");
         // $test = shell_exec('echo "Hello world" > /del/null &');
-        dd($authResponse);
+        dd($authResponse . env('ROOT_DIR') . "authorize {$mainDomain} {$additionalDomains} -n");
 
         $successMessage = "The authorization tokens was successfully fetched!";
 
